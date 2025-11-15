8d956cbf08ec5483ae47bd97e57d9bb93381fc7d
Dahsiee <chuksgabriella2019@gmail.com>
2025-11-15 10:01:17 +0000

    fix: format import statement in PostModal component for consistency

--- alx-project-0x01/components/common/PostModal.tsx
+++ alx-project-0x01/components/common/PostModal.tsx
@@ -1,4 +1,5 @@
-import { PostData, PostModalProps} from "@/interfaces";
+import { PostData, PostModalProps } from "@/interfaces";
+
 import React, { useState } from "react";
 
 const PostModal: React.FC<PostModalProps> = ({onClose, onSubmit}) => {
